be857d86ea86b6d65be787880fd1320542e225aa
Waleed Khan <arxanas@gmail.com>
2014-12-17 19:17:01 -0500

    Feature: Added setup script to install as package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+if __name__ == "__main__":
+    setup(
+        name="schedumich",
+        packages=["schedumich"],
+        version="0.0.1",
+        description="Schedule classes using the University of Michigan API.",
+        author="Waleed Khan",
+        author_email="me@waleedkhan.name",
+        url="https://github.com/arxanas/schedumich",
+        license="MIT"
+    )
